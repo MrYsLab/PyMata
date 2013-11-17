ca2eca653eb165ba1d61e02f48c2606ca3d631b5
MrYsLab <MiterYsLab@gmail.com>
2013-11-17 09:04:19 -0500

    initial entry

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='PyMata',
     packages=['PyMata'],
-    version='1.0',
+    version='1.1',
     description="A Python Protocol Abstraction Library For Arduino Firmata",
     author='Alan Yorinks',
     author_email='MisterYsLab@gmail.com',
